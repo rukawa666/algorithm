ac6be2fe6d6dc8b1ffe5ebba57c1cefc92f45e60
hanqinghai <rukawa.666@outlook.com>
2021-08-02 17:07:25 +0800

    进阶1

--- src/com/rukawa/algorithm/leetcode/top100likedquestions/Problem_0307_RangeSumQueryMutable.java
+++ src/com/rukawa/algorithm/leetcode/top100likedquestions/Problem_0307_RangeSumQueryMutable.java
@@ -10,8 +10,8 @@ package com.rukawa.algorithm.leetcode.top100likedquestions;
 public class Problem_0307_RangeSumQueryMutable  {
     /**
      * 区域和检索 - 数组可修改
-     * 给定一个整数数组  nums，求出数组从索引 i 到 j  (i ≤ j) 范围内元素的总和，包含 i,  j 两点。
-     * update(i, val) 函数可以通过将下标为 i 的数值更新为 val，从而对数列进行修改。
+     * 给定一个整数数组 nums，求出数组从索引i到j(i≤j) 范围内元素的总和，包含i, j两点。
+     * update(i, val) 函数可以通过将下标为i的数值更新为val，从而对数列进行修改。
      *
      * 示例:
      * Given nums = [1, 3, 5]
@@ -20,7 +20,7 @@ public class Problem_0307_RangeSumQueryMutable  {
      * sumRange(0, 2) -> 8
      *
      * 说明:
-     * 数组仅可以在 update 函数下进行修改。
+     * 数组仅可以在update函数下进行修改。
      * 你可以假设 update 函数与 sumRange 函数的调用次数是均匀分布的。
      */
     class NumArray {
--- src/com/rukawa/algorithm/types/advanced/Code_A6_AOE.java
+++ src/com/rukawa/algorithm/types/advanced/Code_A6_AOE.java
@@ -1,5 +1,7 @@
 package com.rukawa.algorithm.types.advanced;
 
+import java.util.Arrays;
+
 /**
  * @className: Code_A6_AOE
  * @description: TODO 类描述
@@ -14,4 +16,302 @@ public class Code_A6_AOE {
      * 被打到的每只怪兽损失1点血量，
      * 返回要把所有怪兽血量清空，至少需要释放多少次AOE技能？
      */
+    // 纯暴力解法
+    public static int minAoe1(int[] x, int[] hp, int range) {
+        int N = x.length;
+        int[] coverLeft = new int[N];
+        int[] coverRight = new int[N];
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < N; i++) {
+            while (x[i] - x[left] > range) {
+                left++;
+            }
+            while (right < N && x[right] - x[i] <= range) {
+                right++;
+            }
+            coverLeft[i] = left;
+            coverRight[i] = right - 1;
+        }
+        return process(hp, coverLeft, coverRight);
+    }
+
+    public static int process(int[] hp, int[] coverLeft, int[] coverRight) {
+        int N = hp.length;
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            for (int f = coverLeft[i]; f <= coverRight[i]; f++) {
+                if (hp[f] > 0) {
+                    int[] next = aoe(hp, coverLeft[i], coverRight[i]);
+                    ans = Math.min(ans, 1 + process(next, coverLeft, coverRight));
+                    break;
+                }
+            }
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+
+    public static int[] aoe(int[] hp, int L, int R) {
+        int N = hp.length;
+        int[] next = new int[N];
+        for (int i = 0; i < N; i++) {
+            next[i] = hp[i];
+        }
+        for (int i = L; i <= R; i++) {
+            next[i] -= next[i] > 0 ? 1 : 0;
+        }
+        return next;
+    }
+
+    // 贪心策略：永远让最左边缘以最优的方式（AOE尽可能的往右扩，让最左边缘盖住怪的最左）变成0，也就是选择
+    // 一定要覆盖最左边缘，但是尽量靠右的中心点
+    // 等到最左边缘变成0之后，再去找下一个最左边缘
+    public static int minAoe2(int[] x, int[] hp, int range) {
+        int N = x.length;
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            if (hp[i] > 0) {
+                int triggerPost = i;
+                while (triggerPost < N && x[triggerPost] - x[i] <= range) {
+                    triggerPost++;
+                }
+                ans += hp[i];
+                aoe(x, hp, i, triggerPost - 1, range);
+            }
+        }
+        return ans;
+    }
+
+    public static void aoe(int[] x, int[] hp, int L, int trigger, int range) {
+        int N = x.length;
+        int RPost = trigger;
+        while (RPost < N && x[RPost] - x[trigger] <= range) {
+            RPost++;
+        }
+        int minus = hp[L];
+        for (int i = 0; i < RPost; i++) {
+            hp[i] = Math.max(0, hp[i] - minus);
+        }
+    }
+
+    // 线段树
+    // O(N*logN)
+    public static int minAoe3(int[] x, int[] hp, int range) {
+        /**
+         * [5, 7, 6, 4, 2, 9, 5, 4]
+         * 贪心：关注最边缘处的位置，消掉边缘处的5最少需要5次，让AOE最左侧的边缘碰到5，消5次，[0, 2, 1, -1, 2, 9, 5, 4]
+         * 然后卡住2，继续释放2次，[0, 0, -1, -3, 0, 9, 5, 4]
+         * 最后卡住9，释放9次技能，最终全部消掉
+         */
+        int N = x.length;
+        int[] coverLeft = new int[N + 1];
+        int[] coverRight = new int[N + 1];
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < N; i++) {
+            while (x[i] - x[left] > range) {
+                left++;
+            }
+            while (right < N && x[right] - x[i] <= range) {
+                right++;
+            }
+            coverLeft[i + 1] = left + 1;
+            coverRight[i + 1] = right;
+        }
+        int[] best = new int[N + 1];
+        int trigger = 0;
+        for (int i = 0; i < N; i++) {
+            while (trigger < N && x[trigger] - x[i] <= range) {
+                trigger++;
+            }
+            best[i + 1] = trigger;
+        }
+        SegmentTree st = new SegmentTree(hp);
+        st.build(1, N, 1);
+        int ans = 0;
+        for (int i = 1; i <= N; i++) {
+            long leftEdge =  st.query(i, i, 1, N, 1);
+            if (leftEdge > 0) {
+                ans += leftEdge;
+                int t = best[i];
+                int l = coverLeft[t];
+                int r = coverRight[t];
+                st.add(1, r, (int) (-leftEdge), 1, N, 1);
+            }
+        }
+        return ans;
+
+    }
+
+    public static class SegmentTree {
+        private int MAXN;
+        private int[] arr;
+        private int[] sum;
+        private int[] lazy;
+        private int[] change;
+        private boolean[] update;
+
+        public SegmentTree(int[] origin) {
+            MAXN = origin.length + 1;
+            arr = new int[MAXN];
+            for (int i = 1; i < MAXN; i++) {
+                arr[i] = origin[i - 1];
+            }
+            sum = new int[MAXN << 2];
+            lazy = new int[MAXN << 2];
+            change = new int[MAXN << 2];
+            update = new boolean[MAXN << 2];
+        }
+
+        private void pushUp(int rt) {
+            sum[rt] = sum[rt << 1] + sum[rt << 1 | 1];
+        }
+
+        private void pushDown(int rt, int ln, int rn) {
+            if (update[rt]) {
+                update[rt << 1] = true;
+                update[rt << 1 | 1] = true;
+
+                change[rt << 1] = change[rt];
+                change[rt << 1 | 1] = change[rt];
+
+                lazy[rt << 1] = 0;
+                lazy[rt << 1 | 1] = 0;
+
+                sum[rt << 1] = change[rt] * ln;
+                sum[rt << 1 | 1] = change[rt] * rn;
+                update[rt] = false;
+            }
+
+            if (lazy[rt] != 0) {
+                lazy[rt << 1] += lazy[rt];
+                lazy[rt << 1 | 1] += lazy[rt];
+                sum[rt << 1] += lazy[rt] * ln;
+                sum[rt << 1 | 1] += lazy[rt] * rn;
+                lazy[rt] = 0;
+            }
+        }
+
+
+        public void build(int l, int r, int rt) {
+            if (l == r) {
+                sum[rt] = arr[l];
+                return;
+            }
+            int mid = (l + r) >> 1;
+            build(l, mid, rt << 1);
+            build(mid + 1, r, rt << 1 | 1);
+            pushUp(rt);
+        }
+
+        public void add(int L, int R, int C, int l, int r, int rt) {
+            if (L <= l && r <= R) {
+                sum[rt] += (r - l + 1) * C;
+                lazy[rt] += C;
+                return;
+            }
+            int mid = (l + r) >> 1;
+            pushDown(rt, mid - l + 1, r - mid);
+            if (L <= mid) {
+                add(L, R, C, l, mid, rt << 1);
+            }
+
+            if (R > mid) {
+                add(L, R, C, mid + 1, r, rt << 1 | 1);
+            }
+            pushUp(rt);
+        }
+
+        public void update(int L, int R, int C, int l, int r, int rt) {
+            if (L <= l && r <= R) {
+                change[rt] = C;
+                update[rt]  = true;
+                lazy[rt] = 0;
+                sum[rt] = C * (r - l + 1);
+                return;
+            }
+            int mid = (l + r) >> 1;
+            pushDown(rt, mid - l + 1, r - mid);
+            if (L <= mid) {
+                update(L, R, C, l, mid, rt << 1);
+            }
+
+            if (R > mid) {
+                update(L, R, C, mid + 1, r, rt << 1 | 1);
+            }
+            pushUp(rt);
+        }
+
+        public long query(int L, int R, int l, int r, int rt) {
+            if (L <= l && r <= R) {
+                return sum[rt];
+            }
+            int mid = (l + r) >> 1;
+            pushDown(rt, mid - l + 1, r - mid);
+            long ans = 0;
+            if (L <= mid) {
+                ans += query(L, R, l, mid, rt << 1);
+            }
+
+            if (R > mid) {
+                ans += query(L, R, mid + 1, r, rt << 1 | 1);
+            }
+            return ans;
+        }
+    }
+
+    // for test
+    public static int[] randomArray(int n, int valueMax) {
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = (int) (Math.random() * valueMax) + 1;
+        }
+        return ans;
+    }
+
+    // for test
+    public static int[] copyArray(int[] arr) {
+        int N = arr.length;
+        int[] ans = new int[N];
+        for (int i = 0; i < N; i++) {
+            ans[i] = arr[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int N = 500;
+        int X = 10000;
+        int H = 50;
+        int R = 10;
+        int time = 5000;
+        System.out.println("test begin");
+        for (int i = 0; i < time; i++) {
+            int len = (int) (Math.random() * N) + 1;
+            int[] x = randomArray(len, X);
+            Arrays.sort(x);
+            int[] hp = randomArray(len, H);
+            int range = (int) (Math.random() * R) + 1;
+            int[] x2 = copyArray(x);
+            int[] hp2 = copyArray(hp);
+            int ans2 = minAoe2(x2, hp2, range);
+            // 已经测过下面注释掉的内容，注意minAoe1非常慢，
+            // 所以想加入对比需要把数据量(N, X, H, R, time)改小
+//			int[] x1 = copyArray(x);
+//			int[] hp1 = copyArray(hp);
+//			int ans1 = minAoe1(x1, hp1, range);
+//			if (ans1 != ans2) {
+//				System.out.println("Oops!");
+//				System.out.println(ans1 + "," + ans2);
+//			}
+            int[] x3 = copyArray(x);
+            int[] hp3 = copyArray(hp);
+            int ans3 = minAoe3(x3, hp3, range);
+            if (ans2 != ans3) {
+                System.out.println("Oops!");
+                System.out.println(ans2 + "," + ans3);
+            }
+        }
+        System.out.println("test end");
+    }
 }

ce78f0209cb220ea429e9f45f4412ac84ca5b556
rukawa666 <rukawa666@outlook.com>
2021-08-11 07:05:51 +0800

    进阶3

--- src/com/rukawa/algorithm/types/advanced/Code_C5_MinBoat.java
+++ src/com/rukawa/algorithm/types/advanced/Code_C5_MinBoat.java
@@ -7,4 +7,11 @@ package com.rukawa.algorithm.types.advanced;
  * @date: 2021/8/9 0009 21:41
  **/
 public class Code_C5_MinBoat {
+    /**
+     * 给定一个正整数数组arr，代表若干人的体重
+     * 再给定一个正数limit，表示所有船共同拥有的载重量
+     * 每艘船最多坐俩人，且不能超过载重
+     * 想让所有人同时过河，并且拥有最好的分配方法让船尽量少
+     * 返回最少的船数？
+     */
 }

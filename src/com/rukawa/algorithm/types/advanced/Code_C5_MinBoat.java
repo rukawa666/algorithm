9ecea44e3cf98c29e96150461d32cfff3bd6690e
hanqinghai <rukawa.666@outlook.com>
2021-08-21 15:36:21 +0800

    进阶4

--- src/com/rukawa/algorithm/types/advanced/Code_C5_MinBoat.java
+++ src/com/rukawa/algorithm/types/advanced/Code_C5_MinBoat.java
@@ -1,5 +1,7 @@
 package com.rukawa.algorithm.types.advanced;
 
+import java.util.Arrays;
+
 /**
  * @className: Code_C5_MinBoat
  * @description: TODO 类描述
@@ -14,5 +16,48 @@ public class Code_C5_MinBoat {
      * 想让所有人同时过河，并且拥有最好的分配方法让船尽量少
      * 返回最少的船数？
      */
+    public static int minBoat(int[] arr, int limit) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        int N = arr.length;
+        Arrays.sort(arr);
+        if (arr[N - 1] > limit) {
+            return -1;
+        }
+        // 找到有序数组中，左边离limit/2最近的位置
+        int lessR = -1;
+        for (int i = N - 1; i >= 0 ; i--) {
+            if (arr[i] <= (limit >> 1)) {
+                lessR = i;
+                break;
+            }
+        }
+        if (lessR == -1) {
+            return N;
+        }
+        int L = lessR;
+        int R = lessR + 1;
+        // 俩俩匹配使用的船的数量
+        int noUsed = 0;
+        while (L >= 0) {
+            int solved = 0;
+            while (R < N && arr[L] + arr[R] <= limit) {
+                R++;
+                solved++;
+            }
+            if (solved == 0) {
+                noUsed++;
+                L--;
+            } else {
+                L = Math.max(-1, L - solved);
+            }
+
+        }
+        int all = lessR + 1;
+        int used = all - noUsed;
+        int moreUnsolved = (N - all) - used;
+        return used + ((noUsed + 1) >> 1) + moreUnsolved;
+    }
 
 }

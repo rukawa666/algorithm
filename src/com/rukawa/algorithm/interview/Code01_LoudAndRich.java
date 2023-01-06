154e69640fd0425fc3b90f7c33cc3c563fba7d14
hanqinghai <rukawa.666@outlook.com>
2023-01-06 19:32:41 +0800

    851

--- src/com/rukawa/algorithm/interview/Code01_LoudAndRich.java
+++ src/com/rukawa/algorithm/interview/Code01_LoudAndRich.java
@@ -1,5 +1,7 @@
 package com.rukawa.algorithm.interview;
 
+import java.util.ArrayList;
+
 /**
  * create by hqh on 2023/1/5
  */
@@ -15,7 +17,58 @@ public class Code01_LoudAndRich {
      * 现在，返回一个整数数组answer作为答案，其中answer[x]=y的前提是：
      * 在所有拥有的钱肯定不少于person x的人中，person y是最安静的人（也就是安静值quiet[y]最小的人）
      */
+    // richer[i]={a,b} a比b更有钱
+    // quiet[i] = k，这个人安静值是k
     public int[] loudAndRich(int[][] richer, int[] quiet) {
-        return null;
+        int N = quiet.length;
+        // 有三条边
+        // a -> b
+        // a -> c
+        // b -> c
+        // 从a出发的点能的点
+        // a : b c
+        // b : c
+        ArrayList<ArrayList<Integer>> nextS = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            nextS.add(new ArrayList<>());
+        }
+        // 入度
+        int[] degree = new int[N];
+        for (int[] r : richer) {
+            nextS.get(r[0]).add(r[1]);
+            degree[r[1]]++;
+        }
+        // 所有入度为0的点，入队列
+        int[] zeroQueue = new int[N];
+        int l = 0;
+        int r = 0;
+        for (int i = 0; i < N; i++) {
+            if (degree[i] == 0) {
+                zeroQueue[r++] = i;
+            }
+        }
+        // ans[i]=j，比i有钱的所有人里面，j最安静
+        int[] ans = new int[N];
+        for (int i = 0; i < N; i++) {
+            ans[i] = i;
+        }
+
+        while (l < r) { // 如果队列不为空
+            // 弹出一个入度位0的点
+            int cur = zeroQueue[l++];
+            // 消除当前cur的影响
+            for (int next : nextS.get(cur)) {
+                // cur：比cur有钱，最安静的 ans[cur]
+
+                if (quiet[ans[next]] > quiet[ans[cur]]) {
+                    ans[next] = ans[cur];
+                }
+                // 入度--
+                if (--degree[next] == 0) {
+                    zeroQueue[r++] = next;
+                }
+            }
+        }
+        return ans;
     }
 }
